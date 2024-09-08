311c7586ef0250f8d3234a415ef76d49ea18f607
rsteen <reuben.steen@student.unimelb.edu.au>
2024-09-08 20:08:56 +1000

    changed reference from letter to deliverable in a MailRoom method

--- src/MailRoom.java
+++ src/MailRoom.java
@@ -57,9 +57,9 @@ public abstract class MailRoom {
     protected void loadRobot(int floor, Robot robot) {
         ListIterator<Deliverable> iter = waitingForDelivery[floor].listIterator();
         while (iter.hasNext()) {  // In timestamp order
-            Deliverable letter = iter.next();
-            if(robot.getCapacity() >= robot.getUsedCapacity() + letter.myWeight()) {
-                robot.add(letter); //Hand it over
+            Deliverable deliverable = iter.next();
+            if(robot.getCapacity() >= robot.getUsedCapacity() + deliverable.myWeight()) {
+                robot.add(deliverable); //Hand it over
                 iter.remove();
             }
         }
